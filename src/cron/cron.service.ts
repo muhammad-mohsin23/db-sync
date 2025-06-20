b8a65f0d7f8a526fe8a51f7d7a220affd9a6afe5
Muhammad Mohsin <muhammad.mohsin@koderlabs.com>
2025-06-20 22:27:13 +0500

    feat: bug fixes

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,4 +1,5 @@
-import { mysqlConnection } from "../database/database.service";
+import { mysqlConnection, pgPool } from "../database/database.service";
+import { LogError, LogSuccess } from "../logging/logging";
 import {
   insertBooking,
   insertBookingActivity,
@@ -15,46 +16,371 @@ import {
   insertCustomerToAccount,
   updateCustomerInAccount,
 } from "../services/customer";
-import { insertInvoice, insertInvoiceItem } from "../services/invoice";
+import {
+  insertInvoice,
+  insertInvoiceItem,
+  updateInvoice,
+  updateInvoiceItem,
+} from "../services/invoice";
+import { insertPropertyUnit } from "../services/unit";
+const EventEmitter = require("node:events");
+
+const eventEmitter = new EventEmitter();
+
+const batchSize = {
+  create: 5000,
+  update: 25000,
+  delete: 25000,
+};
+
 export async function fetchData() {
-  const currentDate = new Date().setSeconds(0);
+  // const currentDate = new Date().setSeconds(0);
+  const toDelete: number[] = [];
+  const toSoftDelete: Record<string, string>[] = [];
+  const mysqlConn = await mysqlConnection();
+  const createBatchSize = batchSize.create;
   try {
-    const mysqlConn = await mysqlConnection();
-    const [rows] = await mysqlConn.execute(
-      `SELECT * FROM change_log
-WHERE created_at >= DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:00') - INTERVAL 3 MINUTE`
-    );
-    console.log(rows);
-    const [result] = await mysqlConn.execute(
-      `SELECT DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:00') AS formatted_time`
-    );
-    console.log("Formatted MySQL time:", result);
-    for (const row of rows as any[]) {
-      const newData = row.new_data;
-      console.log("New Data:", newData);
-      if (
-        row.action_type === "INSERT" &&
-        row.table_name in insertFunctionsByTablename
-      ) {
-        await insertFunctionsByTablename[row.table_name]?.(newData);
-      } else if (
-        row.action_type === "UPDATE" &&
-        row.table_name in updateFunctionsByTablename
-      ) {
-        await updateFunctionsByTablename[row.table_name]?.(newData);
-      } else if (
-        row.action_type === "DELETE" &&
-        row.table_name in deleteFunctionsByTablename
-      ) {
-        await deleteFunctionsByTablename[row.table_name]?.(newData);
+    const [rowCount] = (await mysqlConn.execute(`SELECT count(*) as count
+      FROM change_log
+      WHERE 
+   action_type = 'INSERT'
+          AND table_name in ('bookings', 'customer')
+       AND deleted_at IS NULL;`)) as any;
+    const total = rowCount[0]?.count;
+    for (let i = 0; i <= total / createBatchSize; i++) {
+      const [rows] = await mysqlConn.execute(
+        // `SELECT * FROM change_log
+        // WHERE created_at >= DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:00')  - INTERVAL 3 MINUTE and created_at < DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:00')`
+        // `SELECT * FROM change_log WHERE deleted_at is null order by Id`
+        // `SELECT * FROM change_log
+        // WHERE created_at >= ${new Date().toISOString()} - INTERVAL 3 MINUTE and deleted_at is null`
+
+        // `SELECT *
+        //     FROM change_log
+        //     WHERE
+        //     created_at >= '2025-06-12T00:00:00.000Z'
+        // AND created_at < '2025-06-13T00:00:00.000Z'
+        // AND table_name in ('bookings', 'bookingservicedetails', 'customer' ,'bookingfeedback', 'bookingaddons', 'bookingactivity','invoices','invoicelineitems')
+        //      AND deleted_at IS NULL
+        //     ORDER BY  limit ${batchSize} offset ${batchSize * i};`
+
+        `SELECT *
+            FROM change_log
+            WHERE
+         table_name in ('bookings', 'customer')
+        AND action_type ='INSERT'
+             AND deleted_at IS NULL
+            ORDER BY created_at limit ${batchSize} offset ${
+          createBatchSize * i
+        };`
+
+        //   `SELECT *
+        // FROM change_log
+        // WHERE
+        // record_id='1414464'
+        //  AND deleted_at IS NULL
+        // ORDER BY Id limit ${batchSize} offset ${batchSize * i};`
+        //       `SELECT *
+        // FROM ebdb.change_log
+        // WHERE table_name = 'invoices' AND action_type ='UPDATE' AND record_id='1463122'`
+
+        //       `SELECT *
+        // FROM ebdb.change_log
+        // WHERE table_name = 'bookingservicedetails' AND action_type ='UPDATE' AND record_id='1356041' order by id `
+      );
+      console.log(`Fetched Rows: `, rows);
+      // const [result] = await mysqlConn.execute(
+      //   `SELECT DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:00') AS formatted_time`
+      // );
+      // console.log("Formatted MySQL time:", result);
+      for (const row of rows as any[]) {
+        try {
+          const newData = row.new_data;
+          console.log("New Data:", newData);
+          if (
+            row.action_type === "INSERT" &&
+            row.table_name in insertFunctionsByTablename
+          ) {
+            await insertFunctionsByTablename[row.table_name]?.(
+              newData,
+              mysqlConn
+            );
+          } else {
+            throw new Error(
+              `Action type ${row.action_type} not supported for table ${row.table_name}`
+            );
+          }
+          toDelete.push(row.Id);
+          eventEmitter.emit("log-success", row);
+        } catch (error: any) {
+          console.error(`Error processing row with ID ${row.Id}:`, error);
+          // Optionally, you can push the row to an error array for further investigation
+          toSoftDelete.push({ id: row.Id, error: error.message });
+          eventEmitter.emit("log-error", { id: row.Id, error: error.message });
+        }
       }
     }
-    console.log("Current Date:", new Date(currentDate).toISOString());
-    await mysqlConn.end();
   } catch (err) {
     console.error("Error fetching data:", err);
+  } finally {
+    if (toDelete.length > 0) {
+      const mysqlConn = await mysqlConnection();
+      try {
+        // Delete processed rows from change_log
+        await mysqlConn.execute(
+          `DELETE FROM change_log WHERE id IN (${toDelete
+            .map((x) => `'${x}'`)
+            .join()})`
+        );
+
+        console.log(`Deleted ${toDelete.length} rows from change_log.`);
+      } catch (error) {
+        console.error("Error deleting rows from change_log:", error);
+      } finally {
+        await mysqlConn.end();
+      }
+    }
+
+    if (toSoftDelete.length > 0) {
+      const mysqlConn = await mysqlConnection();
+      try {
+        // Soft delete processed rows from change_log
+        await mysqlConn.execute(
+          `UPDATE change_log SET deleted_at = NOW() WHERE id IN (${toSoftDelete
+            .map((x) => `'${x.id}'`)
+            .join()})`
+        );
+        console.log(
+          `Soft deleted ${
+            toSoftDelete.length
+          } rows from change_log ${toSoftDelete
+            .map((x) => `'${x.id}'`)
+            .join()}}.`
+        );
+      } catch (error) {
+        console.error("Error soft deleting rows from change_log:", error);
+      } finally {
+        console.error(
+          "======================= Migration Completed ==========================="
+        );
+        await mysqlConn.end();
+      }
+    }
   }
 }
+
+export async function fetchUpdateData() {
+  // const currentDate = new Date().setSeconds(0);
+  const toDelete: number[] = [];
+  const toSoftDelete: Record<string, string>[] = [];
+  const updateBatchSize = batchSize.update;
+  const mysqlConn = await mysqlConnection();
+  try {
+    const [rowCount] = (await mysqlConn.execute(`SELECT count(*) as count
+      FROM change_log
+      WHERE 
+       action_type ='UPDATED'
+          AND table_name in ('bookings', 'bookingservicedetails', 'customer' ,'bookingfeedback', 'bookingaddons', 'bookingactivity','invoices','invoicelineitems')
+       AND deleted_at IS NULL;`)) as any;
+    const total = rowCount[0]?.count;
+    for (let i = 0; i <= total / updateBatchSize; i++) {
+      const [rows] = await mysqlConn.execute(
+        `SELECT *
+            FROM change_log
+            WHERE
+         table_name in ('bookings', 'bookingservicedetails', 'customer' ,'bookingfeedback', 'bookingaddons', 'bookingactivity','invoices','invoicelineitems')
+        AND action_type = 'UPDATE'
+             AND deleted_at IS NULL
+            ORDER BY created_at limit ${batchSize} offset ${
+          updateBatchSize * i
+        };`
+      );
+      console.log(`Fetched Rows: `, rows);
+
+      for (const row of rows as any[]) {
+        try {
+          const newData = row.new_data;
+          console.log("New Data:", newData);
+          if (row.table_name in updateFunctionsByTablename) {
+            await updateFunctionsByTablename[row.table_name]?.(
+              newData,
+              mysqlConn,
+              row.Id
+            );
+          } else {
+            throw new Error(
+              `Action type ${row.action_type} not supported for table ${row.table_name}`
+            );
+          }
+          toDelete.push(row.Id);
+          eventEmitter.emit("log-success", row);
+        } catch (error: any) {
+          console.error(`Error processing row with ID ${row.Id}:`, error);
+          // Optionally, you can push the row to an error array for further investigation
+          toSoftDelete.push({ id: row.Id, error: error.message });
+          eventEmitter.emit("log-error", { id: row.Id, error: error.message });
+        }
+      }
+      // console.log("Current Date:", new Date(currentDate).toISOString());
+      // await mysqlConn.end();
+    }
+  } catch (err) {
+    console.error("Error fetching data:", err);
+  } finally {
+    if (toDelete.length > 0) {
+      const mysqlConn = await mysqlConnection();
+      try {
+        // Delete processed rows from change_log
+        await mysqlConn.execute(
+          `DELETE FROM change_log WHERE id IN (${toDelete
+            .map((x) => `'${x}'`)
+            .join()})`
+        );
+
+        console.log(`Deleted ${toDelete.length} rows from change_log.`);
+        // LogSuccess(toDelete);
+      } catch (error) {
+        console.error("Error deleting rows from change_log:", error);
+      } finally {
+        await mysqlConn.end();
+      }
+    }
+
+    if (toSoftDelete.length > 0) {
+      const mysqlConn = await mysqlConnection();
+      try {
+        // Soft delete processed rows from change_log
+        await mysqlConn.execute(
+          `UPDATE change_log SET deleted_at = NOW() WHERE id IN (${toSoftDelete
+            .map((x) => `'${x.id}'`)
+            .join()})`
+        );
+        console.log(
+          `Soft deleted ${
+            toSoftDelete.length
+          } rows from change_log ${toSoftDelete
+            .map((x) => `'${x.id}'`)
+            .join()}}.`
+        );
+        // LogError(toSoftDelete);
+      } catch (error) {
+        console.error("Error soft deleting rows from change_log:", error);
+      } finally {
+        console.error(
+          "======================= Migration Completed ==========================="
+        );
+        await mysqlConn.end();
+      }
+    }
+  }
+}
+
+export async function fetchDeleteData() {
+  // const currentDate = new Date().setSeconds(0);
+  const toDelete: number[] = [];
+  const toSoftDelete: Record<string, string>[] = [];
+  const mysqlConn = await mysqlConnection();
+  const deleteBatchSize = batchSize.delete;
+
+  try {
+    const [rowCount] = (await mysqlConn.execute(`SELECT count(*) as count
+      FROM change_log
+      WHERE 
+      table_name in ('bookings', 'bookingservicedetails', 'customer' ,'bookingfeedback', 'bookingaddons', 'bookingactivity','invoices','invoicelineitems')
+  AND action_type ='DELETE'
+       AND deleted_at IS NULL;`)) as any;
+    const total = rowCount[0]?.count;
+    for (let i = 0; i <= total / deleteBatchSize; i++) {
+      const [rows] = await mysqlConn.execute(
+        `SELECT *
+            FROM change_log
+            WHERE
+             action_type ='DELETE'
+        AND table_name in ('bookings', 'bookingservicedetails', 'customer' ,'bookingfeedback', 'bookingaddons', 'bookingactivity','invoices','invoicelineitems')
+             AND deleted_at IS NULL
+            ORDER BY created_at limit ${batchSize} offset ${
+          deleteBatchSize * i
+        };`
+      );
+      console.log(`Fetched Rows: `, rows);
+      for (const row of rows as any[]) {
+        try {
+          const newData = row.new_data;
+          console.log("New Data:", newData);
+
+          if (row.table_name in deleteFunctionsByTablename) {
+            await deleteFunctionsByTablename[row.table_name]?.(
+              newData,
+              mysqlConn
+            );
+          } else {
+            throw new Error(
+              `Action type ${row.action_type} not supported for table ${row.table_name}`
+            );
+          }
+          toDelete.push(row.Id);
+          eventEmitter.emit("log-success", row);
+        } catch (error: any) {
+          console.error(`Error processing row with ID ${row.Id}:`, error);
+          // Optionally, you can push the row to an error array for further investigation
+          toSoftDelete.push({ id: row.Id, error: error.message });
+          eventEmitter.emit("log-error", { id: row.Id, error: error.message });
+        }
+      }
+      // console.log("Current Date:", new Date(currentDate).toISOString());
+      // await mysqlConn.end();
+    }
+  } catch (err) {
+    console.error("Error fetching data:", err);
+  } finally {
+    if (toDelete.length > 0) {
+      const mysqlConn = await mysqlConnection();
+      try {
+        // Delete processed rows from change_log
+        await mysqlConn.execute(
+          `DELETE FROM change_log WHERE id IN (${toDelete
+            .map((x) => `'${x}'`)
+            .join()})`
+        );
+
+        console.log(`Deleted ${toDelete.length} rows from change_log.`);
+        // LogSuccess(toDelete);
+      } catch (error) {
+        console.error("Error deleting rows from change_log:", error);
+      } finally {
+        await mysqlConn.end();
+      }
+    }
+
+    if (toSoftDelete.length > 0) {
+      const mysqlConn = await mysqlConnection();
+      try {
+        // Soft delete processed rows from change_log
+        await mysqlConn.execute(
+          `UPDATE change_log SET deleted_at = NOW() WHERE id IN (${toSoftDelete
+            .map((x) => `'${x.id}'`)
+            .join()})`
+        );
+        console.log(
+          `Soft deleted ${
+            toSoftDelete.length
+          } rows from change_log ${toSoftDelete
+            .map((x) => `'${x.id}'`)
+            .join()}}.`
+        );
+        // LogError(toSoftDelete);
+      } catch (error) {
+        console.error("Error soft deleting rows from change_log:", error);
+      } finally {
+        console.error(
+          "======================= Migration Completed ==========================="
+        );
+        await mysqlConn.end();
+      }
+    }
+  }
+}
+
 const insertFunctionsByTablename: Record<string, Function> = {
   customers: insertCustomerToAccount,
   bookings: insertBooking,
@@ -62,16 +388,30 @@ const insertFunctionsByTablename: Record<string, Function> = {
   bookingfeedback: insertBookingFeedback,
   bookingservicedetails: insertBookingServiceDetails,
   bookingactivity: insertBookingActivity,
-  invoice: insertInvoice,
-  invoiceitems: insertInvoiceItem,
+  invoices: insertInvoice,
+  invoicelineitems: insertInvoiceItem,
+  units: insertPropertyUnit,
 };
+
 const updateFunctionsByTablename: Record<string, Function> = {
   customers: updateCustomerInAccount,
   bookings: updateBooking,
   bookingaddons: updateBookingAddOn,
   bookingfeedback: updateBookingFeedback,
   bookingservicedetails: updateBookingServiceDetails,
+  invoices: updateInvoice,
+  invoicelineitems: updateInvoiceItem,
 };
 const deleteFunctionsByTablename: Record<string, Function> = {
   customers: deleteCustomerInAccount,
 };
+
+eventEmitter.on("log-success", (data: any) => {
+  console.log("started log-success");
+  LogSuccess([].concat(data));
+});
+
+eventEmitter.on("log-error", (data: any) => {
+  console.log("started log-error");
+  LogError([].concat(data));
+});
